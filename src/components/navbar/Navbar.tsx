a025b08243dae5eaeeacef35bd35e914b6c99d54
Oussama Mangoub <mangoub4@gmail.com>
2026-06-11 00:48:26 +0100

    Setup language select feature

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,16 +4,42 @@ import { navbarHeight } from 'src/utils/constants';
 import { useEffect, useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
+import LanguageIcon from '@mui/icons-material/Language';
 import { useTranslation } from 'react-i18next';
 // import MovieIcon from '@mui/icons-material/Movie';
 // import LiveTvIcon from '@mui/icons-material/LiveTv';
 // import InfoIcon from '@mui/icons-material/Info';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import i18n from 'src/assets/locales/i18n';
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const theme = useTheme();
   const isMdUp = useMediaQuery(theme.breakpoints.up('md'));
   const { t } = useTranslation();
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLanguageChange = (code: string) => {
+    // Implement language change logic here
+    i18n.changeLanguage(code);
+    setAnchorEl(null);
+  };
+
+  const languageOptions = [
+    { code: 'en', label: 'English' },
+    { code: 'fr', label: 'Français' },
+    { code: 'ar', label: 'العربية' },
+  ];
 
   useEffect(() => {
     if (isMdUp && openMenu) setOpenMenu(false);
@@ -62,31 +88,48 @@ const Navbar = () => {
             {/* Search Bar */}
             <Box
               sx={{
-                p: theme => theme.spacing(0.5, 1.5),
                 display: 'flex',
-                alignItems: 'center',
-                width: { xs: '100%', sm: 300, md: 300 },
-                backgroundColor: colors.phantomBlack,
-                borderColor: colors.primary.main,
-                borderWidth: 1,
-                borderStyle: 'solid',
-                borderRadius: theme => theme.spacing(1),
               }}
             >
-              <IconButton
-                aria-label='nav-menu'
-                aria-expanded={openMenu}
-                aria-controls={openMenu ? 'mobile-nav' : undefined}
-                sx={{ display: { xs: 'flex', md: 'none' } }}
-                onClick={() => setOpenMenu(prev => !prev)}
+              <Box
+                sx={{
+                  p: theme => theme.spacing(0.5, 1.5),
+                  display: 'flex',
+                  alignItems: 'center',
+                  width: { xs: '100%', sm: 300, md: 300 },
+                  backgroundColor: colors.phantomBlack,
+                  borderColor: colors.primary.main,
+                  borderWidth: 1,
+                  borderStyle: 'solid',
+                  borderRadius: theme => theme.spacing(1),
+                }}
               >
-                <MenuIcon color='primary' />
-              </IconButton>
+                <IconButton
+                  aria-label='nav-menu'
+                  aria-expanded={openMenu}
+                  aria-controls={openMenu ? 'mobile-nav' : undefined}
+                  sx={{ display: { xs: 'flex', md: 'none' } }}
+                  onClick={() => setOpenMenu(prev => !prev)}
+                >
+                  <MenuIcon color='primary' />
+                </IconButton>
 
-              <InputBase placeholder={t('searchStreamLens')} sx={{ ml: 1, flex: 1 }} />
-              <IconButton type='button' aria-label='search'>
-                <SearchIcon color='primary' />
+                <InputBase placeholder={t('searchStreamLens')} sx={{ ml: 1, flex: 1 }} />
+                <IconButton type='button' aria-label='search'>
+                  <SearchIcon color='primary' />
+                </IconButton>
+              </Box>
+              {/* Language Selector */}
+              <IconButton type='button' aria-label='language' onClick={handleClick}>
+                <LanguageIcon color='primary' />
               </IconButton>
+              <Menu sx={{ color: colors.primary.main }} anchorEl={anchorEl} open={open} onClose={handleClose}>
+                {languageOptions.map(option => (
+                  <MenuItem key={option.code} onClick={() => handleLanguageChange(option.code)}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </Menu>
             </Box>
           </Box>
         </Toolbar>
